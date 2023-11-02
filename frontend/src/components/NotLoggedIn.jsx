352bd43b80d2552fc0d46b5a47c60272f55d1b58
Erik Ehrström <erik.ehrstrom@gmail.com>
2023-11-03 00:25:59 +0200

    Add not logged in component

---
+++ frontend/src/components/NotLoggedIn.jsx
@@ -0,0 +1,35 @@
+import {useNavigate} from "react-router-dom";
+import {useState} from "react";
+
+
+/**
+ * NotLoggedIn is used at pages where the user must be authenticated, but is not.
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const NotLoggedIn = () => {
+
+  const [isSuccess, setIsSuccess] = useState(true);
+  const [isError, setIsError] = useState(true);
+
+  const handleGenerateDBData = () => {
+  }
+
+
+  const navigate = useNavigate();
+
+  const handleNotLoggedIn = () => {
+    navigate('/login')
+  }
+
+  return(
+    <div>
+      <h1 className="text-4xl mb-8">You are not logged in!</h1>
+        <div className="flex flex-col items-center justify-center">
+          <button className="btn btn-wide btn-neutral mb-2" onClick={handleNotLoggedIn}>Go to log in</button>
+        </div>
+    </div>
+  )
+}
+
+export default NotLoggedIn
