efce1bd807f0eea71db57785a50fdcfce1e8eae4
Erik Ehrström <erik.ehrstrom@gmail.com>
2023-11-02 10:37:08 +0200

    Update routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import {BrowserRouter} from "react-router-dom";
-import AppRouter from "./AppRouter.jsx";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <AppRouter/>
+    <App/>
   </BrowserRouter>
 )
