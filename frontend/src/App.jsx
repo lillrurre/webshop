69a95d8932069d5373c8223d01d5ad33d5442dda
Erik Ehrström <erik.ehrstrom@gmail.com>
2023-11-02 10:37:03 +0200

    Update routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {Route, Routes} from "react-router-dom";
+import Login from "./components/login.jsx";
+import Shop from "./components/shop.jsx";
+import Signup from "./components/Signup.jsx";
+import {UserProvider} from "./components/UserContext.jsx";
+
+const App = () => {
+
+  return (
+    <div>
+      <UserProvider>
+        <Routes>
+          <Route path={"/"} element={<Shop />}></Route>
+          <Route path={"/login"} element={<Login />}></Route>
+          <Route path={"/signup"} element={<Signup />}></Route>
+        </Routes>
+      </UserProvider>
+    </div>
+  )
+};
+
+export default App;
