ca95de063a7c96d99c5ed66c000460818ac7d0be
Erik Ehrström <erik.ehrstrom@gmail.com>
2023-11-03 00:24:44 +0200

    Add not case for not logged in

--- frontend/src/components/Account.jsx
+++ frontend/src/components/Account.jsx
@@ -1,9 +1,24 @@
-import { useContext } from 'react';
-import { UserContext } from '../UserContext';
+import {useContext, useState} from 'react';
+import NotLoggedIn from "./NotLoggedIn.jsx";
+import {UserContext} from "./UserContext.jsx";
+import SeedDatabase from "./SeedDatabase.jsx";
 
 const Account = () => {
   const { user } = useContext(UserContext);
-  console.log(user)
+
+  if (user === null) {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <NotLoggedIn/>
+        <SeedDatabase/>
+      </div>
+    )
+  }
+  return (
+    <div>
+
+    </div>
+  )
 };
 
 export default Account;
