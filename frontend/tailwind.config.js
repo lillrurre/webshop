2d9af49b1ba18eb3209332ca3f90d79c722a7145
Erik Ehrström <erik.ehrstrom@gmail.com>
2023-11-06 19:33:59 +0200

    Add tailwind CSS config with default theme 'forest'

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: ["forest", "pastel"]
+  },
+  plugins: [
+    require("daisyui"),
+  ],
+}
+
