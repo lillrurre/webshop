88ee334b41cf70f596a93b0e7c9c0472f70a893e
Erik Ehrström <erik.ehrstrom@gmail.com>
2023-11-06 19:29:22 +0200

    Fetch user

--- frontend/src/components/UserContext.jsx
+++ frontend/src/components/UserContext.jsx
@@ -1,25 +1,61 @@
-import { createContext, useState } from 'react';
+import {createContext, useContext, useState} from 'react';
 import PropTypes from "prop-types";
+import Cookie from 'js-cookie'
+import {AlertContext} from "./AlertContext.jsx";
 
 const UserContext = createContext(undefined);
 
 const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null)
 
-  // Define functions to set and clear the user when they log in or out.
+  const { setError } = useContext(AlertContext)
+
   const login = (userData) => {
-    setUser(userData);
-    console.log(user)
+    fetch('http://localhost:8000/api/login/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRFToken': Cookie.get('csrftoken'),
+      },
+      body: JSON.stringify(userData),
+      credentials: 'include',
+    }).then(res => {
+      if (res.ok) {
+        return res.json()
+      }
+      throw new Error('Log in failed')
+    }).then((userData) => {
+      setUser(userData)
+    }).catch((err) => {
+      setError('Username or password is incorrect')
+      console.error(err)
+    })
   };
 
   const logout = () => {
-    setUser(null);
+    fetch('http://localhost:8000/api/logout/', {
+      method: 'POST',
+      headers: {
+        'X-CSRFToken': Cookie.get('csrftoken'),
+      },
+      credentials: 'include',
+    }).then(res => {
+      if (!res.ok) {
+        throw new Error('Log out failed')
+      }
+    }).then(() => {
+      setUser(null)
+    }).catch(() => {
+      setError('Log out failed')
+    })
   };
 
   return (
-    <UserContext.Provider value={{ user, login, logout }}>
-      {children}
-    </UserContext.Provider>
+    <div>
+      <UserContext.Provider value={{ user, login, logout }}>
+        {children}
+      </UserContext.Provider>
+    </div>
   );
 };
 
