641388d9ca544da0eeda187b32f8b725e6a7bce6
Erik Ehrström <erik.ehrstrom@gmail.com>
2023-11-07 21:02:20 +0200

    move things around

--- shop/urls.py
+++ shop/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
     path('user/update/', views.update_password, name='update_password'),
     path('database/populate/', views.populate_database, name='populate_database'),
     path('item/create/', views.insert_item, name='insert_item'),
-    path('item/purchase/', views.purchase_item, name='purchase_item'),
     path('item/update/', views.update_item, name='update_item'),
     path('items/', views.get_items, name='get_all_items'),
     path('items/own/', views.get_own_items, name='get_own_items'),
+    path('items/purchase/', views.purchase_items, name='purchase_items'),
     path('items/<str:title>/', views.get_items_by_title, name='get_items_by_title'),
 ]
