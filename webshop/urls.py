13a2ce073f061b53ed9d04f252467c4b8b677c06
Erik Ehrström <erik.ehrstrom@gmail.com>
2023-11-07 21:04:11 +0200

    Add url that catches all requests

--- webshop/urls.py
+++ webshop/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('shop.urls')),  # Include shop app's URLs
-    path('', TemplateView.as_view(template_name='index.html'))
+    # Serve all routes, starting from '/'
+    re_path(r'', TemplateView.as_view(template_name='index.html'))
 ]
